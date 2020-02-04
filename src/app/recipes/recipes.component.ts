dcd21525ddb3436504b1e9469d4cd436a4a766cd
Shikho Roy <shikhor@eatlbd.com>
2020-02-04 14:30:00 +0600

    Create Components

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepies',
+  templateUrl: './reiepes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
