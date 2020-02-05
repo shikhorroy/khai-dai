4081e04f961377c77b9e4d9857b812e792af3227
Shikho Roy <shikhor@eatlbd.com>
2020-02-05 15:25:17 +0600

    add recipe list item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,12 @@ export class RecipeListComponent implements OnInit {
   recipes = [
     new Recipe('The test recipe', 'This is a simply a test',
       'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend' +
+      '.hgtvcom.826.620.suffix/1565115622965.jpeg'),
+    new Recipe('The test recipe', 'This is a simply a test',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend' +
+      '.hgtvcom.826.620.suffix/1565115622965.jpeg'),
+    new Recipe('The test recipe', 'This is a simply a test',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend' +
       '.hgtvcom.826.620.suffix/1565115622965.jpeg')
   ];
 
